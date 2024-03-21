71c0c3c1c648389121d3b70675d75a61a317bad7
Muhammad Faizan Soomro <mfaizansoomro00@gmail.com>
2024-03-22 03:16:28 +0500

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,8 +6,8 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      "/api": "https://swole-society-api.vercel.app/",
-      "/uploads": "https://swole-society-api.vercel.app/",
+      "/api": "https://swole-society-api.vercel.app",
+      "/uploads": "https://swole-society-api.vercel.app",
     },
   },
 })
